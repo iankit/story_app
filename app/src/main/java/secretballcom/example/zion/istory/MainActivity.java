f667e768ab2ec37219fcc67cf00d2221c25031e2
Ankit <iankit17@gmail.com>
2015-08-13 23:38:02 +0530

    Some Functions Added

--- app/src/main/java/secretballcom/example/zion/istory/MainActivity.java
+++ app/src/main/java/secretballcom/example/zion/istory/MainActivity.java
@@ -1,10 +1,12 @@
 package secretballcom.example.zion.istory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mName;
@@ -17,8 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         mName = (EditText)findViewById(R.id.nameText);
         mStartButton=(Button)findViewById(R.id.button);
-        mStartButton.hasOnClickListeners(new View.OnClickListener(){
-        public void onClick()
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mName.getText().toString();
+                Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+                startStory(name);
+
+            }
+        });
+    }
+
+    private void startStory(String name){
+        Intent intent = new Intent(MainActivity.this,StoryActivity.class);
+        intent.putExtra("name",name);
+        startActivity(intent);
     }
 
 }
